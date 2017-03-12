ccc7def4ae5b79153536ae9d7c051123cc6de73d
abravion <abravion@users.noreply.github.com>
2017-03-12 20:49:37 +0100

    Update index.js

--- index.js
+++ index.js
@@ -7,10 +7,10 @@ module.exports = function(homebridge) {
     Characteristic = homebridge.hap.Characteristic;
     UUIDGen = homebridge.hap.uuid;
 
-    homebridge.registerPlatform("homebridge-rademacher-blinds", "RademacherBlinds", RademacherBlinds, true);
+    homebridge.registerPlatform("homebridge-rademacher-switchs", "RademacherSwitchs", RademacherSwitchs, true);
 };
 
-function RademacherBlinds(log, config, api) {
+function RademacherSwitchs(log, config, api) {
     // global vars
     this.log = log;
 
@@ -32,7 +32,7 @@ function RademacherBlinds(log, config, api) {
                 if(e) return new Error("Request failed.");
                 var body = JSON.parse(b);
                 body.devices.forEach(function(data) {
-                    if(data.productName.includes("RolloTron") || data.productName.includes("Troll Comfort") || data.productName.includes("Rohrmotor"))
+                    if(data.productName.includes("Schaltaktor 2-Kanal"))
                     {
                         var uuid = UUIDGen.generate(data.serial);
                         var accessory = self.accessories[uuid];
@@ -42,7 +42,7 @@ function RademacherBlinds(log, config, api) {
                         }
                         else {
                             self.log("Online: %s [%s]", accessory.displayName, data.serial);
-                            self.accessories[uuid] = new RademacherBlindsAccessory(self.log, (accessory instanceof RademacherBlindsAccessory ? accessory.accessory : accessory), data, self.url);
+                            self.accessories[uuid] = new RademacherSwitchsAccessory(self.log, (accessory instanceof RademacherSwitchsAccessory ? accessory.accessory : accessory), data, self.url);
                         }
                     }
                 });
@@ -51,36 +51,36 @@ function RademacherBlinds(log, config, api) {
     }
 }
 
-RademacherBlinds.prototype.configureAccessory = function(accessory) {
+RademacherSwitchs.prototype.configureAccessory = function(accessory) {
     this.accessories[accessory.UUID] = accessory;
 };
 
-RademacherBlinds.prototype.addAccessory = function(blind) {
-    this.log("Found: %s - %s [%s]", blind.name, blind.description, blind.serial);
+RademacherSwitchs.prototype.addAccessory = function(Switch) {
+    this.log("Found: %s - %s [%s]", Switch.name, Switch.description, Switch.serial);
 
     var name = null;
-    if(!blind.description.trim())
-        name = blind.name;
+    if(!Switch.description.trim())
+        name = Switch.name;
     else
-        name = blind.description;
-    var accessory = new Accessory(name, UUIDGen.generate(blind.serial));
+        name = Switch.description;
+    var accessory = new Accessory(name, UUIDGen.generate(Switch.serial));
     accessory.addService(Service.WindowCovering, name);
-    this.accessories[accessory.UUID] = new RademacherBlindsAccessory(this.log, accessory, blind, this.url);
+    this.accessories[accessory.UUID] = new RademacherSwitchsAccessory(this.log, accessory, Switch, this.url);
 
-    this.api.registerPlatformAccessories("homebridge-rademacher-blinds", "RademacherBlinds", [accessory]);
+    this.api.registerPlatformAccessories("homebridge-rademacher-switchs", "RademacherSwitchs", [accessory]);
 };
 
-RademacherBlinds.prototype.removeAccessory = function(accessory) {
+RademacherSwitchs.prototype.removeAccessory = function(accessory) {
     if (accessory) {
         this.log("[" + accessory.description + "] Removed from HomeBridge.");
         if (this.accessories[accessory.UUID]) {
             delete this.accessories[accessory.UUID];
         }
-        this.api.unregisterPlatformAccessories("homebridge-rademacher-blinds", "RademacherBlinds", [accessory]);
+        this.api.unregisterPlatformAccessories("homebridge-rademacher-switchs", "RademacherSwitchs", [accessory]);
     }
 };
 
-function RademacherBlindsAccessory(log, accessory, blind, url) {
+function RademacherSwitchsAccessory(log, accessory, Switch, url) {
     var self = this;
 
     var info = accessory.getService(Service.AccessoryInformation);
@@ -88,17 +88,17 @@ function RademacherBlindsAccessory(log, accessory, blind, url) {
     accessory.context.manufacturer = "Rademacher";
     info.setCharacteristic(Characteristic.Manufacturer, accessory.context.manufacturer.toString());
 
-    accessory.context.model = blind.productName;
+    accessory.context.model = Switch.productName;
     info.setCharacteristic(Characteristic.Model, accessory.context.model.toString());
 
-    accessory.context.serial = blind.serial;
+    accessory.context.serial = Switch.serial;
     info.setCharacteristic(Characteristic.SerialNumber, accessory.context.serial.toString());
 
     this.accessory = accessory;
-    this.blind = blind;
+    this.Switch = Switch;
     this.log = log;
     this.url = url;
-    this.lastPosition = reversePercentage(this.blind.position);
+    this.lastPosition = reversePercentage(this.Switch.position);
     this.currentPositionState = 2;
     this.currentTargetPosition = 100;
 
@@ -106,12 +106,12 @@ function RademacherBlindsAccessory(log, accessory, blind, url) {
 
     this.service
         .getCharacteristic(Characteristic.CurrentPosition)
-        .setValue(reversePercentage(self.blind.position))
+        .setValue(reversePercentage(self.Switch.position))
         .on('get', this.getCurrentPosition.bind(this));
 
     this.service
         .getCharacteristic(Characteristic.TargetPosition)
-        .setValue(reversePercentage(self.blind.position))
+        .setValue(reversePercentage(self.Switch.position))
         .on('get', this.getTargetPosition.bind(this))
         .on('set', this.setTargetPosition.bind(this));
 
@@ -122,7 +122,7 @@ function RademacherBlindsAccessory(log, accessory, blind, url) {
     accessory.updateReachability(true);
 }
 
-RademacherBlindsAccessory.prototype.setTargetPosition = function(value, callback) {
+RademacherSwitchsAccessory.prototype.setTargetPosition = function(value, callback) {
     this.log("%s - Setting target position: %s", this.accessory.displayName, value);
 
     var self = this;
@@ -130,7 +130,7 @@ RademacherBlindsAccessory.prototype.setTargetPosition = function(value, callback
     var moveUp = (this.currentTargetPosition >= this.lastPosition);
     this.service.setCharacteristic(Characteristic.PositionState, (moveUp ? 1 : 0));
 
-    var params = "cid=9&did="+this.blind.did+"&command=1&goto="+reversePercentage(value);
+    var params = "cid=9&did="+this.Switch.did+"&command=1&goto="+reversePercentage(value);
     request.post({
         headers: {'content-type' : 'application/x-www-form-urlencoded'},
         url: this.url,
@@ -147,11 +147,11 @@ RademacherBlindsAccessory.prototype.setTargetPosition = function(value, callback
     });
 };
 
-RademacherBlindsAccessory.prototype.getTargetPosition = function(callback) {
+RademacherSwitchsAccessory.prototype.getTargetPosition = function(callback) {
     this.log("%s - Getting target position", this.accessory.displayName);
 
     var self = this;
-    var serial = this.blind.serial;
+    var serial = this.Switch.serial;
 
     request.get({
         timeout: 1500,
@@ -171,11 +171,11 @@ RademacherBlindsAccessory.prototype.getTargetPosition = function(callback) {
     });
 };
 
-RademacherBlindsAccessory.prototype.getCurrentPosition = function(callback) {
+RademacherSwitchsAccessory.prototype.getCurrentPosition = function(callback) {
     this.log("%s - Getting current position", this.accessory.displayName);
 
     var self = this;
-    var serial = this.blind.serial;
+    var serial = this.Switch.serial;
 
     request.get({
         timeout: 1500,
@@ -194,7 +194,7 @@ RademacherBlindsAccessory.prototype.getCurrentPosition = function(callback) {
     });
 };
 
-RademacherBlindsAccessory.prototype.getPositionState = function(callback) {
+RademacherSwitchsAccessory.prototype.getPositionState = function(callback) {
     callback(null, this.currentPositionState);
 };
 
